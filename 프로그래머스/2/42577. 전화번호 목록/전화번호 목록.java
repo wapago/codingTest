488edc068342562515c3852cdb792b8572e9c453
Kim Chanho <109461257+wapago@users.noreply.github.com>
2024-08-19 16:35:47 +0900

    Docs: 틀린풀이 추가

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -13,3 +13,23 @@ class Solution {
         return true;
     }
 }
+
+// 틀린풀이 추가 : 시간초과
+class Solution {
+    public boolean solution(String[] phone_book) {
+        
+        for(int i=0; i<phone_book.length; i++) {
+            for(int j=0; j<phone_book.length; j++) {
+                if(phone_book[i].equals(phone_book[j])) {
+                    continue;
+                }
+                
+                if(phone_book[i].startsWith(phone_book[j])) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
