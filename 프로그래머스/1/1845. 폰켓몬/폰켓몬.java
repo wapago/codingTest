8a6805d8a9c297a238e551de3fb276588f6e37b8
Kim Chanho <109461257+wapago@users.noreply.github.com>
2024-08-19 12:57:05 +0900

    Update 폰켓몬2.java

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -14,3 +14,20 @@ class Solution {
         return set.size();
     }
 }
+
+// 다른 사람 풀이 1
+class Solution {
+    public int solution(int[] nums) {
+
+            HashSet<Integer> hs = new HashSet<>();
+
+            for(int i =0; i<nums.length;i++) {
+                hs.add(nums[i]);
+            }
+
+            if(hs.size()>nums.length/2)
+                return nums.length/2;
+
+            return hs.size();
+    }
+}
