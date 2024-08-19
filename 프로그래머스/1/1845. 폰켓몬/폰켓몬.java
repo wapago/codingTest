9374999ce79bd12c486112ee867b5a7401091b2f
Kim Chanho <109461257+wapago@users.noreply.github.com>
2024-08-19 12:45:04 +0900

    [level 1] Title: 폰켓몬, Time: 11.28 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int targetNum = nums.length / 2;
+        
+        Integer[] integerArr = Arrays.stream(nums).boxed().toArray(Integer[]::new);
+        Set<Integer> set = new HashSet<Integer>(Arrays.asList(integerArr));
+
+        if(targetNum <= set.size()) {
+            return targetNum;
+        }
+        
+        return set.size();
+    }
+}
