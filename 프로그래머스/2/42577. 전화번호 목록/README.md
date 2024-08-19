635df97b5dfc4ffb559ba36fff016fc636ea80d8
Kim Chanho <109461257+wapago@users.noreply.github.com>
2024-08-19 16:50:42 +0900

    [level 2] Title: 전화번호 목록, Time: 319.50 ms, Memory: 226 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java#) 
 
 ### 성능 요약
 
-메모리: 97.6 MB, 시간: 355.88 ms
+메모리: 226 MB, 시간: 319.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 19일 16:35:00
+2024년 08월 19일 16:50:41
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,34 +2,32 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book);
-        
-        for(int i=0; i<phone_book.length-1; i++) {
-            if(phone_book[i+1].startsWith(phone_book[i])) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-}
-
-// 틀린풀이 추가 : 시간초과
-class Solution {
-    public boolean solution(String[] phone_book) {
+        Map<String, Integer> map = new HashMap<>();
         
         for(int i=0; i<phone_book.length; i++) {
-            for(int j=0; j<phone_book.length; j++) {
-                if(phone_book[i].equals(phone_book[j])) {
-                    continue;
-                }
-                
-                if(phone_book[i].startsWith(phone_book[j])) {
+            map.put(phone_book[i], i);
+        }
+        
+        // 모든 전화번호의 substring이 HashMap에 존재하는지 확인한다.
+        // beginIndex위치에서 시작하여 endIndex 전 위치까지의 값을 리턴한다.
+        for(int i=0; i<phone_book.length; i++) {
+            for(int j=0; j<phone_book[i].length(); j++) {
+                if(map.containsKey(phone_book[i].substring(0, j)))
                     return false;
-                }
             }
         }
         
         return true;
     }
+//     public boolean solution(String[] phone_book) {
+//         Arrays.sort(phone_book);
+        
+//         for(int i=0; i<phone_book.length-1; i++) {
+//             if(phone_book[i+1].startsWith(phone_book[i])) {
+//                 return false;
+//             }
+//         }
+        
+//         return true;
+//     }
 }
