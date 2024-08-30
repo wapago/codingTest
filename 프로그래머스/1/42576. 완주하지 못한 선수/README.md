39861608608e5e8a221ebc89fba236355ccc2c54
Kim Chanho <109461257+wapago@users.noreply.github.com>
2024-08-30 13:24:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 86.44 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 95.3 MB, 시간: 316.09 ms
+메모리: 97.6 MB, 시간: 86.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 20일 12:54:28
+2024년 08월 30일 13:24:21
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,16 +1,24 @@
-import java.util.*;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-	    Arrays.sort(completion);
-	    
-        for(int i=0; i<completion.length; i++){
-           if(!participant[i].equals(completion[i])){
-               return participant[i];
-           }
+        // 해시맵 생성
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        
+        // 완주한 선수들의 이름을 해시맵에 저장
+        for(String string : completion) {
+            hashMap.put(string, hashMap.getOrDefault(string, 0) + 1);
+        }
+        
+        // 참가한 선수들의 이름을 키로 하는 값을 1씩 감소
+        for(String string : participant) {
+            // 완주하지 못한 선수를 찾으면 반환
+            if(hashMap.getOrDefault(string, 0) == 0) {
+                return string;
+            }
+            hashMap.put(string, hashMap.get(string) - 1);
         }
 	    
-        return participant[participant.length-1];
+        return null;
     }   
 }
