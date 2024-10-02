65ea514ef98375d9dff2511501ced2b6fb2d727d
Kim Chanho <109461257+wapago@users.noreply.github.com>
2024-10-02 15:51:52 +0900

    [level 1] Title: 폰켓몬, Time: 46.27 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 81.9 MB, 시간: 16.27 ms
+메모리: 88 MB, 시간: 46.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 19일 12:55:54
+2024년 10월 02일 15:51:49
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,33 +1,21 @@
-import java.util.*;
+import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        int targetNum = nums.length / 2;
-
-        Integer[] integerArr = Arrays.stream(nums).boxed().toArray(Integer[]::new);
-        Set<Integer> set = new HashSet<Integer>(Arrays.asList(integerArr));
-
-        if(targetNum <= set.size()) {
-            return targetNum;
+        int answer = 0;
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num : nums) {
+            System.out.println(num);
+            set.add(num);
         }
-
-        return set.size();
-    }
-}
-
-// 다른 사람 풀이 1
-class Solution {
-    public int solution(int[] nums) {
-
-            HashSet<Integer> hs = new HashSet<>();
-
-            for(int i =0; i<nums.length;i++) {
-                hs.add(nums[i]);
-            }
-
-            if(hs.size()>nums.length/2)
-                return nums.length/2;
-
-            return hs.size();
+        
+        int targetTypeCount = (nums.length / 2);    // 가질 수 있는 종류의 수
+        
+        if(targetTypeCount >= set.size()) return set.size();
+        
+        // return (set.size() * (set.size() - 1)) / 2;
+        return targetTypeCount;
     }
 }
