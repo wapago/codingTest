6b76d6566a49f2fc73882eb9222310bab5eaf489
Kim Chanho <109461257+wapago@users.noreply.github.com>
2024-10-02 15:18:39 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,11 +1,38 @@
 class Solution {
     public int[] solution(int num, int total) {
         int[] answer = new int[num];
-        int start = (total/num) - ((num - 1) / 2);
         
-        for(int i=0; i<num; i++) {
-            answer[i] = start;
-            start++;
+        
+        if(num % 2 != 0) {
+            // total / num 의 값이 가운데 인덱스의 값이 된다.
+            // 가운데 인덱스 : (num / 2) - 1
+            int middleIdx = (num / 2);
+            answer[middleIdx] = total / num;
+            
+            for(int i = middleIdx - 1; i >= 0; i--) {
+                answer[i] = answer[i+1] - 1;
+            }
+            
+            for(int i = middleIdx + 1; i < answer.length; i++) {
+                answer[i] = answer[i-1] + 1;
+            }
+        }
+        
+        // (num / 2)의 값이 가운데 값의 첫번째 값.
+        if(num % 2 == 0) {
+            int middleFirstIdx = (num / 2) - 1;
+            int middleSecondIdx = (num / 2);
+            
+            answer[middleFirstIdx] = total / num;
+            answer[middleSecondIdx] = (total / num) + 1;
+            
+            for(int i = middleFirstIdx - 1; i >= 0; i--) {
+                answer[i] = answer[i+1] - 1;
+            }
+            
+            for(int i = middleSecondIdx + 1; i < answer.length; i++) {
+                answer[i] = answer[i-1] + 1;
+            }
         }
         
         return answer;
