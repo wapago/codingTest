0b7ec758ffbc7ac68f99a6c04665e79d8b397fbf
Kim Chanho <109461257+wapago@users.noreply.github.com>
2024-10-02 15:35:56 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.05 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,25 +1,24 @@
+import java.util.HashSet;
+
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        int[] differenceArr = new int[common.length-1];
-        int difference = 0;
         
-        // 원소의 뺄셈을 해본다
-        for(int i=1; i<common.length; i++) {
-            differenceArr[i-1] = common[i] - common[i-1];
+        int length  = common.length;
+        int[] gap = new int[length-1];        
+        
+        for(int i = 0; i < length - 1; i++) {
+            gap[i] = common[i+1] - common[i];
         }
         
-        // 차이가 같은지 다른지 본다
-        for(int i=0; i<differenceArr.length-1; i++) {
-            if(differenceArr[i] == differenceArr[i+1]) {
-                difference = differenceArr[i];
-                // 같으면 차이만큼 더하여 구한다
-                return common[common.length-1] + difference;
-            }
-            
-            difference = differenceArr[i+1] / differenceArr[i];
+        if(gap[0] == gap[1]) {
+            answer = common[length-1] + gap[0];
         }
         
-        return common[common.length-1] * difference;
+        if(gap[0] != gap[1]) {
+            answer = common[length-1] * (common[1] / common[0]);
+        }
+        
+        return answer;
     }
 }
