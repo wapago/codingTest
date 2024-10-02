53e81c3baa306b1437a906575a7a166cbe6a92d3
Kim Chanho <109461257+wapago@users.noreply.github.com>
2024-10-02 16:05:07 +0900

    [level 1] Title: 폰켓몬, Time: 8.55 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,20 +1,7 @@
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] nums) {
-        // nums리스트에서 중복을 제거한 집합(set)을 구함
-        HashSet<Integer> set = Arrays.stream(nums).boxed()
-                                     .collect(Collectors.toCollection(HashSet::new));
-        
-        // 폰켓몬의 총 수
-        int n = nums.length;
-        
-        // 선택할 폰켓몬의 수
-        int k = n / 2;
-        
-        // 중복을 제거한 폰켓몬의 종류 수와 선택할 폰켓몬의 수 중 작은 값 반환
-        return Math.min(k, set.size());
+        return Math.min((int) Arrays.stream(nums).distinct().count(), nums.length / 2);
     }
 }
