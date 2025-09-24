10ba02406a7bfc3eebba70a9775503dbeff5459d
Kim Chanho <109461257+wapago@users.noreply.github.com>
2025-09-24 21:17:36 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.06 ms, Memory: 9.28 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 택배 상자 꺼내기 - 389478 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478#) 
 
 ### 성능 요약
 
-메모리: 9.33 MB, 시간: 0.03 ms
+메모리: 9.28 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 24일 21:16:42
+2025년 09월 24일 21:17:33
 
 ### 문제 설명
 
--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.py
@@ -1,13 +1,9 @@
 def solution(n, w, num):
     row_count = (n//w) if (n/w) == (n//w) else (n//w) + 1
-    print(f'총 줄개수: {row_count}')
     target_row = (num//w) if (num/w) == (num//w) else (num//w) + 1
-    print(f'타겟 줄: {target_row}')
         
     target_row_start = (w * target_row) if target_row % 2 == 0 else (w * target_row) - (w - 1)
-    print(f'타겟 줄 시작: {target_row_start}')
     target_row_last = target_row_start - (w-1) if target_row % 2 == 0 else target_row_start + (w-1)
-    print(f'타겟 줄 마지막: {target_row_last}')
     
     target_array = []
     
@@ -22,9 +18,6 @@ def solution(n, w, num):
             if target_array[i] == num:
                 target_idx = i
     
-    print(f'target_array: {target_array}')
-    print(f'target_idx: {target_idx}')
-        
     last_row_min = w * (row_count - 1) + 1
     
     last_array = []
@@ -40,8 +33,6 @@ def solution(n, w, num):
         for i in range(w - len(last_array)):
             last_array.append(0)
         
-    print(f'last_array: {last_array}')
-                   
     if last_array[target_idx]:
         return row_count - target_row + 1
     else:
