a660200e04267913e79e0c606bbaee25ccf5c934
Kim Chanho <109461257+wapago@users.noreply.github.com>
2025-10-17 01:11:19 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.35 ms, Memory: 9.14 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -6,15 +6,7 @@ def solution(food):
         if food[i] >= 2:
             first_half += str(i) * (food[i] // 2)
             
-    print(first_half)
-    
     for i in range(len(first_half)-1, -1, -1):
         second_half += first_half[i]
         
-    print(second_half)        
-        
-    answer = first_half + "0" + second_half
-    
-    print(answer)
-    
-    return answer
+    return first_half + "0" + second_half
